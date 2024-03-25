64091cc8aa5814f2182113f1a48552274936d080
Michael Zschutschke <zschutschke@kombinat-berlin.de>
2024-03-25 01:01:06 +0100

    add custom fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,14 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        'primary-green': '#93bfa6',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
